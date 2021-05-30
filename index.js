eb1004dc778697dc818600efd749fdd562729397
Mora-Dav <moradavi@gmail.com>
2021-05-29 21:01:53 -0300

    Contenido html

--- index.js
+++ index.js
@@ -5,4 +5,9 @@ app.listen (3000, () => console.log('Servidor corriendo: https://localhost:3000'
 
 app.get ('/', (req, res) => {
     res.send('¡Hola mundo!');
-})
+});
+
+app.get ('/users', (req, res) => {
+    res.send('Esta es la página de usuarios')
+});
+
